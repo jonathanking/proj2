798c9f03b59a60fe7a74a829ca87ea2309f230bf
jonathanking <jonathanking@berkeley.edu>
2013-11-11 16:24:00 -0800

    Added AITest to the directory and made it so that it does not run the game.

---
+++ jump61/AITest.java
@@ -0,0 +1,33 @@
+package jump61;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import jump61.Player.Move;
+
+import org.junit.Test;
+
+public class AITest {
+
+    @Test
+    public void setUp() throws Exception {
+        Writer output = new OutputStreamWriter(System.out);
+        Game game = new Game(new InputStreamReader(System.in),
+                             output, output,
+                             new OutputStreamWriter(System.err));
+        Board board = new MutableBoard(3);
+        Color who = Color.BLUE;
+        HumanPlayer red = new HumanPlayer(game, Color.RED);
+        AI blue = new AI(game, who);
+        board.addSpot(red.getColor(), 1, 1);
+        Move test = blue.findBestMove(who, board, 1, 200);
+        assertTrue(blue instanceof AI);
+    }
+
+
+
+
+}
